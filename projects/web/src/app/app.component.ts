78ac8f4fbbc3dc14fa7dc4d33d804dc44432e7ba
beginor <beginor@qq.com>
2020-05-30 17:23:59 +0800

    init map when app created.

--- projects/web/src/app/app.component.ts
+++ projects/web/src/app/app.component.ts
@@ -1,13 +1,47 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import {
+    Component, OnInit, Inject, AfterViewInit, ViewChild, ElementRef, OnDestroy
+} from '@angular/core';
+import {
+    trigger, animate, style, state, transition, AnimationEvent
+} from '@angular/animations';
 
-import { prewarm, clearPrewarmedResources } from 'mapbox-gl';
+import { Map, prewarm, clearPrewarmedResources } from 'mapbox-gl';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+    styleUrls: ['./app.component.scss'],
+    animations: [
+        trigger('state', [
+            state('show', style({ left: '0px' })),
+            state('hide', style({ left: '-360px' })),
+            transition('show => hide', animate('.3s ease-in-out')),
+            transition('hide => show', animate('.3s ease-in-out'))
+        ])
+    ]
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements AfterViewInit, OnInit, OnDestroy {
+
+    @ViewChild('mapview', { static: true })
+    public mapElRef: ElementRef<HTMLDivElement>;
+    public state = 'show';
+
+    private map: Map;
+
+    constructor(
+        @Inject('mapboxToken') private mapboxToken: string
+    ) { }
+
+    public ngAfterViewInit(): void {
+        this.map = new Map({
+            container: this.mapElRef.nativeElement,
+            accessToken: this.mapboxToken,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [113.259, 23.132],
+            zoom: 7
+        });
+        window['_mapview'] = this.map;
+    }
 
     public ngOnInit(): void {
         prewarm();
@@ -15,6 +49,26 @@ export class AppComponent implements OnInit, OnDestroy {
 
     public ngOnDestroy(): void {
         clearPrewarmedResources();
+        if (!!this.map) {
+            this.map.remove();
+        }
+    }
+
+    public toggleState(e: Event): void {
+        e.preventDefault();
+        e.stopPropagation();
+        this.state = (this.state === 'show') ? 'hide' : 'show';
+    }
+
+    public getToggleIconClass(): string {
+        let classeName = 'fas fa-chevron-';
+        if (this.state === 'show') {
+            classeName += 'left';
+        }
+        else {
+            classeName += 'right';
+        }
+        return classeName;
     }
 
 }
--- projects/web/src/app/home/home/home.component.ts
+++ projects/web/src/app/home/home/home.component.ts
@@ -1,42 +1,17 @@
 import {
-    Component, OnInit, Inject, AfterViewInit, ViewChild, ElementRef, OnDestroy
+    Component, OnInit
 } from '@angular/core';
-import { Map } from 'mapbox-gl';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements AfterViewInit, OnInit, OnDestroy {
+export class HomeComponent implements OnInit {
 
-    @ViewChild('mapview', { static: true })
-    public mapElRef: ElementRef<HTMLDivElement>;
-
-    private map: Map;
-
-    constructor(
-        @Inject('mapboxToken') private mapboxToken: string
-    ) { }
+    constructor() { }
 
     public ngOnInit(): void {
-    }
-
-    public ngOnDestroy(): void {
-        if (!!this.map) {
-            this.map.remove();
-        }
-    }
-
-    public ngAfterViewInit(): void {
-        this.map = new Map({
-            container: this.mapElRef.nativeElement,
-            accessToken: this.mapboxToken,
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [113.259, 23.132],
-            zoom: 7
-        });
-        window['_mapview'] = this.map;
     }
 
 }
