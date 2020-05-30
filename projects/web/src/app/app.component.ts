8dea7ae13e0072b1e1b029bbc15950d79d27f58b
beginor <beginor@qq.com>
2020-05-30 17:04:41 +0800

    init mapbox

--- projects/web/src/app/app.component.ts
+++ projects/web/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { prewarm, clearPrewarmedResources } from 'mapbox-gl';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+
+    public ngOnInit(): void {
+        prewarm();
+    }
+
+    public ngOnDestroy(): void {
+        clearPrewarmedResources();
+    }
 
 }
--- projects/web/src/app/home/home/home.component.ts
+++ projects/web/src/app/home/home/home.component.ts
@@ -1,19 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-import { HomeService } from './home.service';
+import {
+    Component, OnInit, Inject, AfterViewInit, ViewChild, ElementRef, OnDestroy
+} from '@angular/core';
+import { Map } from 'mapbox-gl';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements AfterViewInit, OnInit, OnDestroy {
 
-    constructor(public vm: HomeService) { }
+    @ViewChild('mapview', { static: true })
+    public mapElRef: ElementRef<HTMLDivElement>;
 
-    public ngOnInit(): void { }
+    private map: Map;
 
-    public onClick(): void {
-        this.vm.updateMessage();
+    constructor(
+        @Inject('mapboxToken') private mapboxToken: string
+    ) { }
+
+    public ngOnInit(): void {
+    }
+
+    public ngOnDestroy(): void {
+        if (!!this.map) {
+            this.map.remove();
+        }
+    }
+
+    public ngAfterViewInit(): void {
+        this.map = new Map({
+            container: this.mapElRef.nativeElement,
+            accessToken: this.mapboxToken,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [113.259, 23.132],
+            zoom: 7
+        });
+        window['_mapview'] = this.map;
     }
 
 }
