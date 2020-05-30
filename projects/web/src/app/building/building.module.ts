b5b343646f855d87952ad9b0fb7ce640322db1cb
beginor <beginor@qq.com>
2020-05-30 17:54:21 +0800

    add building module

---
+++ projects/web/src/app/building/building.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BuildingRoutingModule } from './building-routing.module';
+import { HomeComponent } from './home/home.component';
+
+
+@NgModule({
+    declarations: [HomeComponent],
+    imports: [
+        CommonModule,
+        BuildingRoutingModule
+    ]
+})
+export class BuildingModule { }
